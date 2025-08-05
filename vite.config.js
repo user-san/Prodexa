39dac0420c8093914ae565decb02a67002c944c7
user-san <114083548+user-san@users.noreply.github.com>
2025-08-05 20:01:39 +0530

    viteConfig changed

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Products_Listing/', // 👈 must match your repo name exactly
+  base: '/Prodexa/', // 👈 must match your repo name exactly
   plugins: [react()],
 })
